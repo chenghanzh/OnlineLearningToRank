b60853f71de6f9a9505ca43dcaa52d79901ebfe1
Eric McCord-Snook <esm7ky@virginia.edu>
2019-01-24 00:55:56 -0500

    Found increasing variance ordering of queries for all subsets of the Yahoo dataset

---
+++ scripts/esm7ky/query_variance_sort/query_variance_sort.py
@@ -0,0 +1,36 @@
+# Authored by Eric McCord-Snook
+# January 23, 2019
+import sys
+import numpy as np
+
+def main():
+    args = sys.argv
+    print("Reading input file", args[1], "...")
+    lines = [line.strip() for line in open(args[1])]
+    print("Calculating variances...")
+    qids_and_variances = []
+    current_qid = None
+    feature_matrix = []
+    for line in lines:
+        tokens = line.split(" ")
+        if tokens[1] == current_qid or current_qid == None:
+            current_qid = tokens[1]
+            feature_vector = np.zeros(701)
+            for token in tokens[2:]:
+                f_id, f_val = token.split(":")
+                feature_vector[int(f_id)] = float(f_val)
+            feature_matrix.append(feature_vector)
+        else:
+            net_var_for_query = np.sum(np.var(np.mat(feature_matrix), axis=0))
+            qids_and_variances.append((int(current_qid.split(":")[1]), net_var_for_query))
+            print("Finished", current_qid)
+            current_qid = tokens[1]
+            feature_matrix = []
+    qids_and_variances.sort(key=lambda tup: tup[1])
+
+    with open("set2_test_qid_inc_var.txt", "w") as outfile:
+        for tup in qids_and_variances:
+            outfile.write(str(tup[0]) + "\n")
+
+if __name__ == '__main__':
+    main()
