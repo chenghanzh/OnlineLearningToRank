b3fbc8455954a73b32322d764565ad1d1e52cc94
Sonwoo Kim <sak2km@gmail.com>
2019-01-24 19:34:00 -0500

    nsgd in largemem

--- models/linearmodel.py
+++ models/linearmodel.py
@@ -78,7 +78,7 @@ class LinearModel(object):
 
     # added for projection
     gradient = np.sum(weighted_docs, axis=0)
-    if viewed_list: is not None and len(viewed_list)>0:
+    if viewed_list is not None and len(viewed_list)>0:
       if lambda_intp: # add Linear Interpolation (project only partially)
         gradient = (1-lambda_intp)*gradient + (lambda_intp * self.project_to_viewed_doc(gradient,viewed_list,svd,project_norm))
 
